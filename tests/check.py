ddf1db6e316af229d2071523928b210555442611
Justin Miles <jumiles@paloaltonetworks.com>
2026-03-17 12:08:01 -0700

    removed extra pyproject.toml files

--- tests/check.py
+++ tests/check.py
@@ -1,11 +1,4 @@
 #!/python
-import sys
-import pprint
-# sys.modules is a dictionary; keys are module names
-current_imports = sorted(sys.modules.keys()) 
-pprint.pprint(current_imports) # Uncomment to see the full list
-import repositories
-help(repositories)
 
 from apu.repositories import get_repo
 
