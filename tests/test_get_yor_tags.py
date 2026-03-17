9773f248cb7ab1c59d75838b7e90b73f22c7f213
Justin Miles <jumiles@paloaltonetworks.com>
2026-03-17 13:09:34 -0700

    fixed test cases

--- tests/test_get_yor_tags.py
+++ tests/test_get_yor_tags.py
@@ -1,17 +1,13 @@
 #!/python
 
-import pytest
-
-# from apu.utils import login, http_logging # importing this should trigger the login procedure
-
 from apu.utils import login
 
 login.login()
 
 from apu.yor import get_tags
 
-def my_test():
+def test_get_tags():
     tags = get_tags.get_tags()
     for tag in tags:
         print(tag['name'])
-    return len(tags)
+    assert not len(tags) == 0
