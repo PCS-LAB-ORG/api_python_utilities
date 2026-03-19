c3e5a14cadd92726508cfb190aa6a3e0edd699b8
Justin Miles <jumiles@paloaltonetworks.com>
2026-03-19 09:18:55 -0700

    added core suppression scripts

--- apu/suppressions/core.py
+++ apu/suppressions/core.py
@@ -9,61 +9,109 @@ import requests
 import json
 from apu.utils import login, http_logging # importing this should trigger the login procedure
 
-# login.login(redlock=False) # Authorization header
+login.login(redlock=False) # Authorization header
 
-def get()
-    url = f"{settings['url']}/code/api/v1/suppressions"
+def get():
+    url = f"{login.settings['url']}/code/api/v1/suppressions"
 
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=login.headers)
     js_res = json.loads(response.text)
     # pprint.pprint(js_res)
     return js_res
 
+def create(category):
+    if category == "Secrets":
+        comment = "it\'s elementary"
+        policy_id = "BC_GIT_9" #"902599344975759360_SECRETS_1745427256645"
+        account_id = "jumiles/webgoat"
+        file_path = "/src/test/java/org/owasp/webgoat/webwolf/jwt/JWTTokenTest.java"
+        commit_sha = "f4dac9065a6c93d8d93caf8a9567d9cb83688ba0"
+        # expiration_time = 1756278000000
+
+        payload = {
+            "comment":comment,
+            # "expirationTime":expiration_time,
+            "suppressionType":"Resources",
+            "resources":{
+                "accountId":account_id,
+                "id":f"{policy_id}::{account_id}::{file_path}:{commit_sha}"
+            },
+            "origin":"Platform"
+        }
+
+        url = f"{login.settings['url']}/bridgecrew/api/v1/suppressions/{policy_id}"
+
+        response = requests.request("POST", url, headers=login.headers, data=payload)
+
+        js_res = json.loads(response.text)
+        pprint.pprint(js_res)
+    return js_res
+
+    # curl 'https://api2.prismacloud.io/bridgecrew/api/v1/suppressions/$policyId' \
+    #   -H 'Authorization: ...' \
+    #   -H 'Accept: application/json, text/plain, */*' \
+    #   -H 'Content-Type: application/json' \
+    #   --data-raw $'{"comment":$comment,"expirationTime":1756278000000,"suppressionType":"Resources","resources":{"accountId":"jumiles-pa/test-cas-app","id":"$policyId::jumiles-pa/test-cas-app::/README.md:3ac743df70f6d3b34bceee4753eeeedaa4650911"},"origin":"Platform"}'
+
+def delete(policy, suppression):
+    suppression_id = "" # UUID Format
+    policy_id = "" # like BC_GIT_2
+
+    url = f"{login.settings['url']}/api/v1/suppressions/{policy_id}/justifications/{suppression_id}"
+    response = requests.request("DELETE", url, headers=login.headers)
+    js_res = json.loads(response.text)
+    return js_res
+
+def output_by_category(suppression_list):
+    resources = []
+    tags = []
+    cves = []
+    secrets = []
+    policy = []
+    cve_accounts = []
+    license = []
+    accounts = []
+    for suppression in suppression_list:
+        match suppression["suppressionType"]:
+            case "Resources":
+                resources.append(suppression)
+            case "Tags":
+                tags.append(suppression)
+            case "Cves":
+                cves.append(suppression)
+            case "SecretsPolicy":
+                secrets.append(suppression)
+            case "Policy":
+                policy.append(suppression)
+            case "CvesAccounts":
+                cve_accounts.append(suppression)
+            case "LicenseType":
+                license.append(suppression)
+            case "Accounts":
+                accounts.append(suppression)
+            case _:
+                print(suppression)
+
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    folder_path = f"{cwd}/output"
+    # Create the directory and all parent directories if they don't exist
+    os.makedirs(folder_path, exist_ok=True)
+    with open(f"{cwd}/output/resources.json", "w") as resources_file:
+        json.dump(resources, resources_file, indent=4)
+    with open(f"{cwd}/output/tags.json", "w") as tags_file:
+        json.dump(tags, tags_file, indent=4)
+    with open(f"{cwd}/output/cve.json", "w") as cve_file:
+        json.dump(cves, cve_file, indent=4)
+    with open(f"{cwd}/output/secrets.json", "w") as secrets_file:
+        json.dump(secrets, secrets_file, indent=4)
+    with open(f"{cwd}/output/policy.json", "w") as policy_file:
+        json.dump(policy, policy_file, indent=4)
+    with open(f"{cwd}/output/cve_accounts.json", "w") as cve_accounts_file:
+        json.dump(cve_accounts, cve_accounts_file, indent=4)
+    with open(f"{cwd}/output/license.json", "w") as license_file:
+        json.dump(license, license_file, indent=4)
+    with open(f"{cwd}/output/accounts.json", "w") as accounts_file:
+        json.dump(accounts, accounts_file, indent=4)
+
 suppression_list = get()
-
-resources = []
-tags = []
-cves = []
-secrets = []
-policy = []
-cve_accounts = []
-license = []
-accounts = []
-for suppression in js_res:
-    match suppression["suppressionType"]:
-        case "Resources":
-            resources.append(suppression)
-        case "Tags":
-            tags.append(suppression)
-        case "Cves":
-            cves.append(suppression)
-        case "SecretsPolicy":
-            secrets.append(suppression)
-        case "Policy":
-            policy.append(suppression)
-        case "CvesAccounts":
-            cve_accounts.append(suppression)
-        case "LicenseType":
-            license.append(suppression)
-        case "Accounts":
-            accounts.append(suppression)
-        case _:
-            print(suppression)
-
-cwd = os.path.dirname(os.path.realpath(__file__))
-with open(f"{cwd}/output/resources.json", "w") as resources_file:
-    json.dump(resources, resources_file, indent=4)
-with open(f"{cwd}/output/tags.json", "w") as tags_file:
-    json.dump(tags, tags_file, indent=4)
-with open(f"{cwd}/output/cve.json", "w") as cve_file:
-    json.dump(cves, cve_file, indent=4)
-with open(f"{cwd}/output/secrets.json", "w") as secrets_file:
-    json.dump(secrets, secrets_file, indent=4)
-with open(f"{cwd}/output/policy.json", "w") as policy_file:
-    json.dump(policy, policy_file, indent=4)
-with open(f"{cwd}/output/cve_accounts.json", "w") as cve_accounts_file:
-    json.dump(cve_accounts, cve_accounts_file, indent=4)
-with open(f"{cwd}/output/license.json", "w") as license_file:
-    json.dump(license, license_file, indent=4)
-with open(f"{cwd}/output/accounts.json", "w") as accounts_file:
-    json.dump(accounts, accounts_file, indent=4)
+output_by_category(suppression_list=suppression_list)
---
+++ tests/suppressions/test_suppressions.py
@@ -0,0 +1,13 @@
+#!/python
+
+# from apu.utils import login
+
+# login.login()
+
+from apu.suppressions import core
+
+def test_get(auth_token):
+    suppression_list = core.get()
+    for suppression in suppression_list:
+        print(f"{suppression["suppressionType"]} {suppression["policyId"]} {suppression["comment"]}")
+    assert not len(suppression_list) == 0
