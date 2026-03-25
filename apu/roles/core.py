0b79cf1aeb4df9a37af5fa846da1dd1ac68ff71f
Justin Miles <jumiles@paloaltonetworks.com>
2026-03-25 14:22:40 -0700

    roles and login base files

---
+++ apu/roles/core.py
@@ -0,0 +1,30 @@
+#!/bin/bash python
+# https://pan.dev/prisma-cloud/api/code/sbom-dependencies/
+
+# Prerequisite packages to run this script.
+# pip install pprintpp requests prismacloud-api
+import os
+import json
+from apu.utils import (
+    login,
+    http_logging,
+    constants
+)  # importing this should trigger the login procedure
+
+# http_logging.http_logging()
+
+def list(role):
+    url = f"{login.settings['url']}/user/role"
+    payload = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    response.raise_for_status()
+    return json.loads(response.text)
+
+    # files.list_to_csv(file_name, json_role)
+
+def update_user(user, user_dict=None):
+    if user_dict:
+        user.update(user_dict)
+    pc_api.user_update(user)
+    return user
---
+++ tests/utils/test_login.py
@@ -0,0 +1,65 @@
+import json
+import pytest
+from requests.exceptions import HTTPError
+
+from apu.utils import login, logger
+
+logger = logger.setup_logger()
+
+def test_with_expired_access_key():
+    ''' rotate on failure may be a login flag I want to use '''
+    try:
+        # using a known expired access_key/secret_key
+        # with pytest.raises(HTTPError):
+        login.login(credential_name="expired_credentials")
+        assert login.headers['Authorization'] == "-1" # remove this line once it is expired
+    except Exception as e:
+        logger.info(e)
+
+def test_with_deleted_access_key():
+    ''' rotate on failure may be a login flag I want to use '''
+    try:
+        # using a known deleted access_key/secret_key
+        login.login(credential_name="deleted_credentials")
+    except BaseException as e:
+        if e.response.status_code == 401:
+            js_res = json.loads(e.response.text)
+            assert js_res.get("message") == "invalid_credentials"
+            assert e.response.reason == "Unauthorized"
+
+def test_with_inactive():
+    ''' rotate on failure may be a login flag I want to use '''
+    try:
+        # using a known deleted access_key/secret_key
+        login.login(credential_name="inactive_credentials")
+    except SystemExit as e:
+        assert "401" in e.code
+        assert "inactive_access_key" in e.code
+        
+def test_access_key1():
+    ''' rotate on failure may be a login flag I want to use '''
+    try:
+        # using a known deleted access_key/secret_key
+        login.login(url="https://api2.prismacloud.io/", access_key="", secret_key="")
+        # This successfully logs in
+    except Exception as e:
+        logger.info(e)
+        
+def test_access_key():
+    ''' rotate on failure may be a login flag I want to use '''
+    # using a known deleted access_key/secret_key
+    login.login(url="https://api2.prismacloud.io/", access_key="")
+    # This successfully logs in
+    assert len(login.headers['identity']) == 13
+    
+def test_access_key2():
+    ''' rotate on failure may be a login flag I want to use '''
+    try:
+        # using a known deleted access_key/secret_key
+        login.login(url="https://api2.prismacloud.io/", access_key="as", secret_key="asd")
+    except BaseException as e:
+        if e.response.status_code == 401:
+            js_res = json.loads(e.response.text)
+            assert js_res.get("message") == "invalid_credentials"
+            assert e.response.reason == "Unauthorized"
+        # logger.info(e)
