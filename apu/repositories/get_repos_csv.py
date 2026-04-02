9735922057817ada2d9f175b6d6dc8b05a9654bd
Justin Miles <jumiles@paloaltonetworks.com>
2026-04-02 14:16:35 -0700

    updates in formatting

--- apu/repositories/get_repos_csv.py
+++ apu/repositories/get_repos_csv.py
@@ -3,13 +3,18 @@
 # Prerequisite packages to run this script.
 # pip install requests prismacloud-api
 
-import os
 from datetime import datetime as dt
-from apu.utils import files  # importing this should trigger the login procedure
+from apu.utils import (
+    files,
+    login,
+    constants,
+)  # importing this should trigger the login procedure
 from apu.repositories import get_repo
+
+login.login()
 
 repository_list = get_repo.get_repositories(includeUnmappedProjects=True)
 
-file_name = f"{os.getcwd()}/repos_{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+file_name = f"{constants.log_dir}/repos_{dt.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
 
 files.list_to_csv(file_name, repository_list)
