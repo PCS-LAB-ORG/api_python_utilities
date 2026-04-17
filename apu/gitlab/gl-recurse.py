9c9ff24871ae0a665beb515e30201ef1f77b963e
Justin Miles <jumiles@paloaltonetworks.com>
2026-04-17 14:23:09 -0700

    major updates in implementation. This is now finally a utility library with functions that work as a whole and individually

--- apu/gitlab/gl-recurse.py
+++ apu/gitlab/gl-recurse.py
@@ -1,6 +1,8 @@
 # https://python-gitlab.readthedocs.io/en/stable/index.html
 
+import json
 import csv
+import logging
 import os
 from datetime import datetime as dt
 from pathlib import Path
@@ -8,17 +10,91 @@ from pathlib import Path
 # pip install python-gitlab
 import gitlab
 import gitlab.exceptions
-from gitlab.exceptions import GitlabAuthenticationError
+from gitlab.exceptions import (
+    GitlabAuthenticationError,
+    GitlabHttpError,
+    GitlabListError,
+)
 
 # pip install python-dotenv
 from dotenv import load_dotenv
 
+# pip install requests
+import requests
+from requests.packages import urllib3
+
+enable_public_gitlab_settings = True
+
 script_dir = f"{Path(__file__).resolve().parent}/logs"  # directory of this script
-os.makedirs(script_dir, exist_ok=True)
-
-
 date_time_format_w_seconds = "%Y-%m-%d_%H-%M-%S"
 now = dt.now().strftime(date_time_format_w_seconds)
+log_file_name = f"{script_dir}/output_{now}.log"
+
+ACCESS_LEVELS = {
+    0: "NO_ACCESS",
+    5: "MINIMAL_ACCESS",
+    10: "GUEST_ACCESS",
+    20: "REPORTER_ACCESS",
+    30: "DEVELOPER_ACCESS",
+    40: "MAINTAINER_ACCESS",
+    50: "OWNER_ACCESS",
+}
+
+# https://docs.gitlab.com/api/rest/#offset-based-pagination
+# By documentation this is the upper limit applied to all.
+# It will use this when it automates the pagination
+per_page = 100
+
+# Create a console handler
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+
+def http_logging():
+    """
+    This is to get the deep debugging info out of the gitlab module
+    for http request traffic. A recommended logging config.
+    """
+    # Define a formatter for the log messages
+    formatter = logging.Formatter(
+        "%(levelname)s %(asctime)s %(filename)s:%(lineno)d %(message)s"
+    )
+
+    # Create a console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+
+    urllib3_logger = logging.getLogger("urllib3")
+    urllib3_logger.setLevel(logging.DEBUG)
+    urllib3_logger_handler = logging.FileHandler(log_file_name)
+    urllib3_logger_handler.setLevel(logging.DEBUG)
+    urllib3_logger_handler.setFormatter(formatter)
+    urllib3_logger.addHandler(urllib3_logger_handler)
+    urllib3_logger.addHandler(console_handler)
+    urllib3_logger.propagate = True
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    # Python312\Lib\site-packages\gitlab\_backends\requests_backend.py
+    # Python312\Lib\site-packages\requests\sessions.py
+    gitlab_logger = logging.getLogger("python-gitlab")
+    gitlab_logger.setLevel(logging.DEBUG)
+    gitlab_logger_handler = logging.FileHandler(log_file_name)
+    gitlab_logger_handler.setLevel(logging.DEBUG)
+    gitlab_logger_handler.setFormatter(formatter)
+    gitlab_logger.addHandler(gitlab_logger_handler)
+    gitlab_logger.addHandler(console_handler)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger_handler = logging.FileHandler(log_file_name)
+    logger_handler.setLevel(logging.DEBUG)
+    logger_handler.setFormatter(formatter)
+    logger.addHandler(logger_handler)
+    logger.addHandler(console_handler)
+
+    logger.info(msg=f"{log_file_name = }")
+    return logger
 
 
 def authenticate():
@@ -37,10 +113,11 @@ def authenticate():
     try:
         gl = gitlab.Gitlab(GITLAB_URL, private_token=PRIVATE_TOKEN, ssl_verify=False)
         gl.auth()
-        print(f"Successfully connected to GitLab at {GITLAB_URL}")
+        logger.info(f"Successfully connected to GitLab at {GITLAB_URL}")
+        logger.info("... running ...")
         return gl
     except GitlabAuthenticationError as e:
-        print(f"Error connecting to GitLab: {e}")
+        logger.info(f"Error connecting to GitLab: {e}")
         raise e
 
 
@@ -52,18 +129,23 @@ def get_groups(all_group_objects):
             {
                 "url": group.web_url,
                 "name": group.full_path,
-                "archived": group.archived,
+                "archived": getattr(group, "archived", None),
                 "id": group.id,
-                "ldap_access": group.ldap_access,
-                "ldap_cn": group.ldap_cn,
+                "ldap_access": ACCESS_LEVELS.get(getattr(group, "ldap_access", None)),
+                "ldap_cn": getattr(group, "ldap_cn", None),
             }
         )
     return all_groups
 
 
-def get_members():
+def get_members(gl):
+    # https://docs.gitlab.com/api/users/
     all_members = []
-    for member in list(gl.users.list(get_all=True)):
+    kwargs = {"iterator": True, "per_page": per_page}
+    if enable_public_gitlab_settings:
+        kwargs["timeout"] = 2
+
+    for member in list(gl.users.list(**kwargs)):
         all_members.append(
             {
                 "name": member.name,
@@ -77,117 +159,174 @@ def get_members():
 
 
 def get_members_by_group(group_list):
-    ACCESS_LEVELS = {
-        10: "GUEST_ACCESS",
-        20: "REPORTER_ACCESS",
-        30: "DEVELOPER_ACCESS",
-        40: "MAINTAINER_ACCESS",
-        50: "OWNER_ACCESS",
-    }
+
     grouped_members = []
     for group in group_list:
-        for member in group.members_all.list(all=True):
-            grouped_members.append(
-                {
-                    "name": member.name,
-                    "username": member.username,
-                    "locked": member.locked,
-                    "state": member.state,
-                    "group_id": member.group_id,
-                    "id": member.id,
-                    "access_level": ACCESS_LEVELS.get(member.access_level),
-                }
-            )
+        try:
+            for member in group.members_all.list(all=True, per_page=per_page):
+                grouped_members.append(
+                    {
+                        "name": member.name,
+                        "username": member.username,
+                        "locked": member.locked,
+                        "state": member.state,
+                        "group_id": member.group_id,
+                        "id": member.id,
+                        "access_level": ACCESS_LEVELS.get(member.access_level),
+                    }
+                )
+        except GitlabListError as e:
+            if e.response_code == 403:
+                logger.error(
+                    f"You don't have permission to get the membership info of group {group.name}",
+                )
     return grouped_members
 
 
-def get_projects():
+def get_projects(all_projects):
     """https://python-gitlab.readthedocs.io/en/stable/gl_objects/projects.html"""
-    all_projects = []
-    for project in list(
-        gl.projects.list(membership=True, get_all=True)
-    ):  # Are you a member of all projects?
-        all_projects.append(
+    parsed_projects = []
+    for project in all_projects:
+        parsed_projects.append(
             {
-                "project": project.name,
-                "path": project.path_with_namespace,
-                "url": project.web_url,
-                "id": project.id,
-                "archived": project.archived,
+                "project": project.get("name"),
+                "path": project.get("path_with_namespace"),
+                "url": project.get("web_url"),
+                "id": project.get("id"),
+                "archived": project.get("archived"),
             }
         )
-    return all_projects
+    return parsed_projects
 
 
-def get_ldap_groups():
+def get_ldap_groups(gl):
     ldap_groups = []
-    for group in gl.ldapgroups.list(get_all=True):
-        ldap_groups.append(
-            {
-                "provider": group.provider,
-                "group_access": group.group_access,
-                "cn": group.cn,
-            }
-        )
+    for ldap in list(gl.ldapgroups.list(get_all=True, per_page=per_page)):
+        ldap_groups.append(json.loads(ldap.to_json()))
     return ldap_groups
 
 
 def get_saml_groups(group_list):
     saml_groups = []
     for group in group_list:
-        for saml_group in group.saml_group_links.list(get_all=True):
-            saml_groups.append(
-                {
-                    "saml_group_name": saml_group.saml_group_name,
-                    "access_level": saml_group.access_level,
-                }
-            )
+        for saml_group in list(
+            group.saml_group_links.list(get_all=True, per_page=per_page)
+        ):
+            saml_groups.append(json.loads(saml_group.to_json()))
     return saml_groups
 
 
 def to_csv(filename, arr, keys=None):
+    if not arr:
+        raise IndexError("Empty array")
     # Open a new CSV file for writing
     with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        fieldnames = keys
         if not keys:
             fieldnames = arr[0].keys()
-        else:
-            fieldnames = keys
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerows(arr)
 
 
-gl = authenticate()
+def to_json(filename, arr):
+    if not arr:
+        raise IndexError("Empty array")
+    with open(filename, "w", newline="", encoding="utf-8") as jsonfile:
+        json.dump(arr, jsonfile, indent=4)
 
-group_list = gl.groups.list(all=True)
 
-all_groups = get_groups(group_list)
-to_csv(f"{script_dir}/groups_{now}.csv", all_groups)
+def get_projects_json(gl):
+    """https://python-gitlab.readthedocs.io/en/stable/gl_objects/projects.html"""
+    project_list = []
+    membership = enable_public_gitlab_settings
+    for project in list(
+        gl.projects.list(membership=membership, iterator=True, per_page=per_page)
+    ):
+        project_list.append(json.loads(project.to_json()))
+    return project_list
 
-all_projects = get_projects()
-to_csv(f"{script_dir}/projects_{now}.csv", all_projects)
 
-all_members = get_members()
-to_csv(f"{script_dir}/members_{now}.csv", all_members)
+if __name__ == "__main__":
+    os.makedirs(script_dir, exist_ok=True)
+    logger = http_logging()
 
-grouped_members = get_members_by_group(group_list)
-to_csv(f"{script_dir}/grouped_members_{now}.csv", grouped_members)
+    gl = authenticate()
 
-"""
-These require some higher permissions and just fails and continues if you do not have access.
-This data could be supplimental.
-"""
+    """
+    Since I'm testing against GitLab saas these requests will return 
+    all public data hosted on GitLab which is causing timeouts and errors so when I
+    run it I'll check this setting to adjust accordingly. By default this is made
+    for a private GitLab organization.
+    """
+    enable_public_gitlab_settings = "jumiles@paloaltonetworks.com" in gl.user.email
 
-try:
-    ldap_groups = get_ldap_groups()
-    to_csv(f"{script_dir}/ldap_groups_{now}.csv", ldap_groups)
-except gitlab.exceptions.GitlabListError as e:
-    print("Unable to test without an ldap setup")
-    # raise e
+    """
+    https://docs.gitlab.com/api/groups/#list-all-groups
+    This library wraps the gitlab api referenced here.
+    See this documentation page section for 'active', archived' and 'all_available'
+    ...'all' is a python-gitlab specific parameter that likely is the same as all_available
+    """
+    try:
+        group_list = gl.groups.list(
+            all=True,
+            active=True,
+            archived=False,
+            all_available=not enable_public_gitlab_settings,
+            owned=False,
+        )
+    except gitlab.exceptions.GitlabListError as e:
+        if e.response_code == 500:
+            logger.error(
+                "Justin get a 500 on this since it tries to get all public gitlab.com repos, which is a lot and probably internally blocked on the SaaS."
+            )
 
-try:
-    saml_groups = get_saml_groups(group_list)
-    to_csv(f"{script_dir}/saml_groups_{now}.csv", saml_groups)
-except gitlab.exceptions.GitlabListError as e:
-    print("Unable to test without an ldap setup")
-    # raise e
+    all_groups = get_groups(group_list)
+    to_csv(f"{script_dir}/groups_{now}.csv", all_groups)
+
+    all_projects = get_projects_json(gl)
+    to_json(f"{script_dir}/projects_{now}.json", all_projects)
+
+    all_projects = get_projects(all_projects)
+    to_csv(f"{script_dir}/projects_{now}.csv", all_projects)
+
+    try:
+        all_members = get_members(gl)
+        to_csv(f"{script_dir}/members_{now}.csv", all_members)
+    except requests.exceptions.ReadTimeout as e:
+        logger.error("Justin gets timeouts on this")
+        # logger.error(e)
+
+    try:
+        grouped_members = get_members_by_group(group_list)
+        to_csv(f"{script_dir}/grouped_members_{now}.csv", grouped_members)
+    except GitlabHttpError as e:
+        if e.response_code == 403:
+            logger.error(e)
+        else:
+            raise e
+    except gitlab.exceptions.GitlabListError as e:
+        if e.response_code == 403:
+            logger.error(e)
+        else:
+            raise e
+
+    """
+    These require some higher permissions and just fails and continues if you do not have access.
+    This data could be supplimental.
+    """
+    try:
+        ldap_groups = get_ldap_groups(gl)
+        to_json(f"{script_dir}/ldap_groups_{now}.json", ldap_groups)
+    except gitlab.exceptions.GitlabListError as e:
+        logger.error(e)
+
+    try:
+        saml_groups = get_saml_groups(group_list)
+        to_json(f"{script_dir}/saml_groups_{now}.json", saml_groups)
+    except gitlab.exceptions.GitlabListError as e:
+        logger.error(e)
+    except gitlab.exceptions.GitlabAuthenticationError as e:
+        logger.error(e)
+
+    logger.info(f"Files and logs at {script_dir} with timestamp {now}")
